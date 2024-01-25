c06643091a14c80eac296b08dfecbd1657dad6a9
김종덕 <kjd742388@gmail.com>
2024-01-25 18:01:03 +0900

    20240125 강의장에서 공부

---
+++ 01_python/python08/practice/IO.practice.py
@@ -0,0 +1,67 @@
+# num1 = float(input())
+# num2 = float(input())
+
+# print(type(num1))
+# print(num1+num2)
+
+# 2번 입력 받는다 -> for 사용해서 2번 똑같은 코드
+# result = 0
+# for i in range(2):
+#     num = int(input())
+#     result += num
+# print(result)
+
+# number = int(input())
+# for i in range(number):
+#     print(i+1)
+
+# import sys
+# txt 파일의 각 줄의 내용을
+# input 함수가 실행 될 때마다, 넣어준다.
+# sys.stdin = open('input.txt')
+
+# N = int(input())
+# result = []
+# for i in range(N):
+#     arr = input().split()
+#     # 모든 요소 순회해서 정수로 바꿔서 다시 리스트 만들기
+#     digit_list =[]
+#     for num in arr:
+#         digit_list.append(int(num))
+#     result.append(digit_list)
+# print(result)
+
+# result = []
+# for i in range(N):
+#     arr = list(map(int, input().split()))
+#     result.append(arr)
+# print(result)
+
+# num = int(input())
+# result = 0
+# for i in range(num):
+#     num_list = list(map(int, input().split()))
+#     result += sum(num_list)
+# print(result)
+
+    # print(arr)
+# arr1 = input()
+# arr2 = input()
+# arr3 = input()
+# print(N)
+# print(arr1)
+# print(arr2)
+# print(arr3)
+
+# number = int(input())
+# num_sum = 0
+# while number != 0:
+#     num_sum += number
+#     number -= 1
+# print(num_sum)
+
+# number = int(input())
+
+# for i in range(1, number+1):
+#     print('*'*i)
+
