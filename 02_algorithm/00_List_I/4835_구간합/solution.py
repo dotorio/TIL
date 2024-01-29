a382292aa734afd121b7f8eb00adc6151834a5e6
김종덕 <kjd742388@gmail.com>
2024-01-29 17:52:19 +0900

    20240129 강의장에서 공부2

--- 02_algorithm/00_List_I/4835_구간합/solution.py
+++ 02_algorithm/00_List_I/4835_구간합/solution.py
@@ -1,2 +1,14 @@
 import sys
-sys.stdin = open('sample_input.txt')
+sys.stdin = open('sample_input.txt')\
+
+T = int(input())
+for case in range(T):
+    N, M = map(int, input().split())
+    number_list = list(map(int, input().split()))
+    new_list = []
+    for number in range(N-M+1):
+        number_sum = 0
+        for new_num in range(M):
+            number_sum += number_list[number+new_num]
+        new_list.append(number_sum)
+    print(f'#{case+1} {max(new_list)-min(new_list)}')
