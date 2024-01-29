5fdf4abef816d0587897a04ed768c82953cf387e
김종덕 <kjd742388@gmail.com>
2024-01-29 20:44:57 +0900

    20240129 집에서 공부

---
+++ 01_python/notebooks/a.py
@@ -0,0 +1,6 @@
+girls = ['jane', 'ashley', 'a']
+boys = ['peter', 'jay']
+pair = zip(girls, boys)
+
+print(pair) 
+print(list(pair)) 
---
+++ algorithm_study/colored_paper.py
@@ -0,0 +1,13 @@
+paper_num = int(input())
+arr = [[0 for j in range(1001)] for i in range(1001)]
+for i in range(paper_num):
+    start_x, start_y, width, length = map(int, input().split())
+    for x in range(width):
+        for y in range(length):
+            arr[start_y + y][start_x + x] = i + 1
+for i in range(paper_num):
+    count = 0
+    for z in range(1001):
+        count += arr[z].count(i + 1)
+    print(count)
+    
