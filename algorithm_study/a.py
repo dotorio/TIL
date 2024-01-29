4b407bad3ec5ce9fe07788ea06e1c6622b0f36d4
김종덕 <kjd742388@gmail.com>
2024-01-30 00:18:48 +0900

    20240129 집에서 공부2

---
+++ algorithm_study/a.py
@@ -0,0 +1,2 @@
+array_1001x1001 = [[0] * 1001 for _ in range(1001)]
+print(array_1001x1001)
--- algorithm_study/colored_paper.py
+++ algorithm_study/colored_paper.py
@@ -1,13 +1,14 @@
 paper_num = int(input())
-arr = [[0 for j in range(1001)] for i in range(1001)]
+arr = [[0] * 1001 for _ in range(1001)]
+# 1001 * 1001 배열을 만든다
 for i in range(paper_num):
     start_x, start_y, width, length = map(int, input().split())
     for x in range(width):
         for y in range(length):
             arr[start_y + y][start_x + x] = i + 1
+            # for문으로 색종이를 하나씩 덮는다.
 for i in range(paper_num):
-    count = 0
-    for z in range(1001):
-        count += arr[z].count(i + 1)
+    count = sum(row.count(i + 1) for row in arr)
     print(count)
+    # for문으로 배열 안의 색종이 넘버를 카운트해서 각각 넓이를 출력한다.
     
