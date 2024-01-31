c480946f7f98e0f7e12eb28e09d2ae5c1db0a963
김종덕 <kjd742388@gmail.com>
2024-02-01 07:23:46 +0900

    20240131 집에서 공부2

--- algorithm_study/other/sequence.py
+++ algorithm_study/other/sequence.py
@@ -22,18 +22,22 @@
 N = int(input())
 seq = list(map(int, input().split()))
 max_length = 1
-for i in range(N):
+i = 0
+while i < N:
     seq_length = 1
     while i+1 < N and seq[i] <= seq[i+1]:
         seq_length += 1
         i = i+1
     max_length = max(max_length, seq_length)
-for i in range(N):
+    i = i+1
+i = 0
+while i < N:
     seq_length = 1
     while i+1 < N and seq[i] >= seq[i+1]:
         seq_length += 1
         i = i+1
     max_length = max(max_length, seq_length)
+    i = i+1
 print(max_length)
 
 # N = int(input())
