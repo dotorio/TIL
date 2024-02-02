27f7f9a6dfbe6aeff3966913f0e57a7b7024a1f8
김종덕 <kjd742388@gmail.com>
2024-02-02 17:57:38 +0900

    20240202 강의장에서 공부

--- algorithm_study/other/bingo.py
+++ algorithm_study/other/bingo.py
@@ -1,20 +1,41 @@
 arr = [list(map(int, input().split())) for _ in range(5)]
 arr2 = [list(map(int, input().split())) for _ in range(5)]
+bingo_arr = [[0] * 5 for _ in range(5)]
 cnt = 0
 
-def bingo(num, y, x):
+def bingo(arr):
+    if sum(arr[i][i] for i in range(5)) == 5:
+        return True
+    if sum(arr[4-i][i] for i in range(5)) == 5:
+        return True
     for i in range(5):
+        if sum(arr[i]) == 5:
+            return True
+    for i in range(5):
+        row_sum = 0
         for j in range(5):
-            if arr[i][j] == num:
-                arr[i][j] = 0
-    one_length = 0
-    while 
-    return True
-
+            row_sum += arr[j][i]
+        if row_sum == 5:
+            return True    
+    return False
 
 for i in range(5):
     for j in range(5):
-        cnt += 1
-        if bingo(arr2[i][j], i, j) == True:
-            print(cnt)
-            quit()
+        for i2 in range(5):
+            for j2 in range(5):
+                if arr[i2][j2] == arr2[i][j]:
+                    bingo_arr[i2][j2] = 1
+                    cnt += 1
+                    if bingo(bingo_arr) == True:
+                        print(cnt)
+                        break
+                    break
+            if bingo(bingo_arr) == True:
+                break
+        if bingo(bingo_arr) == True:
+            break
+    if bingo(bingo_arr) == True:
+        break
+
+            
+
