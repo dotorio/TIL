caf4603671e280545cbcf53f899ec92dac8b81ae
김종덕 <kjd742388@gmail.com>
2024-02-13 23:52:23 +0900

    20240213 집에서 공부3

--- 02_algorithm/algorithm05/calcul2/sol.py
+++ 02_algorithm/algorithm05/calcul2/sol.py
@@ -1,12 +1,19 @@
 import sys
 sys.stdin = open('input.txt')
 
-icp = {'+':1,'*':2}
-
 for tc in range(1, 11):
     N = int(input())
     formula = input()
-    post = []
     stack = []
-    for i in formula:
+    i = 0
+    while i < N:
+        if formula[i].isdigit():
+            stack.append(int(formula[i]))
+            i += 1
+        elif formula[i] == '*':
+            stack.append(stack.pop() * int(formula[i+1]))
+            i += 2
+        else:
+            i += 1
+    print(f'#{tc} {sum(stack)}')
         
