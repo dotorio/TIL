0fe0e3830fab00cf95023aa7c35857aef7547614
김종덕 <kjd742388@gmail.com>
2024-02-15 17:41:07 +0900

    20240215 강의장에서 공부

--- algorithm_study/other/2447_star10/sol.py
+++ algorithm_study/other/2447_star10/sol.py
@@ -1,26 +1,40 @@
 # N = int(input())
 # arr = [['*'] * N for _ in range(N)]
-
-# def f(arr):
-#     len_arr = len(arr)
-#     if len_arr == 1:
-#         return
+#
+# def f(arr, A, B, C, D):
+#     if B-A == 1:
+#         return arr
 #     else:
 #         for i in range(3):
 #             for j in range(3):
 #                 if i == 1 and j == 1:
-#                     for x in range(len_arr//3, len_arr//3*2):
-#                         for y in range(len_arr//3, len_arr//3*2):
+#                     for x in range(A+(B-A)//3, A+(B-A)//3*2):
+#                         for y in range(C+(D-C)//3, C+(D-C)//3*2):
 #                             arr[x][y] = ' '
+#                     return arr
 #                 else:
-#                     new_arr = []
-#                     for x in range(len_arr//3*i, len_arr//3*(i+1)):
-#                         new_arr.append(arr)
-# f(arr)
+#                     return f(arr, A+(B-A)//3*i, A+(B-A)//3*(i+1), C+(D-C)//3*j, C+(D-C)//3*(j+1))
+# f(arr, 0, N, 0, N)
 # for i in arr:
 #     print("".join(i))
-a = [[1,2,3],[3,4,5]]
-b = a[0][1:]
-b[0]= 10
-print(b)
-print(a)
+
+N = int(input())
+arr = [['*'] * N for _ in range(N)]
+
+def f(arr, A, B, C, D):
+    if B-A == 1:
+        return arr
+    else:
+        for i in range(3):
+            for j in range(3):
+                if i == 1 and j == 1:
+                    for x in range(A+(B-A)//3, A+(B-A)//3*2):
+                        for y in range(C+(D-C)//3, C+(D-C)//3*2):
+                            arr[x][y] = ' '
+                else:
+                    f(arr, A+(B-A)//3*i, A+(B-A)//3*(i+1), C+(D-C)//3*j, C+(D-C)//3*(j+1))
+        return arr
+
+f(arr, 0, N, 0, N)
+for i in arr:
+    print("".join(i))
