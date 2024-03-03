f79c757ffa22578fd07414ce0699d307b3bc1cf7
김종덕 <kjd742388@gmail.com>
2024-03-04 07:48:19 +0900

    20240303 집에서 공부

--- algorithm_study/swea/jottomatte/sol2.py
+++ algorithm_study/swea/jottomatte/sol2.py
@@ -1,6 +1,6 @@
 import sys
 sys.stdin = open('input.txt')
-from collections import deque
+from queue import Queue
 
 dy = [0, -1, 1, 0, 0]
 dx = [0, 0, 0, -1, 1]
@@ -13,12 +13,13 @@ pipe = {1 : [1, 2, 3, 4],
         6 : [0, 2, 3, 0],
         7 : [1, 0, 3, 0]}
 
-def escape(N, M, R, C, L, tunnel):
+def run(N, M, R, C, L, tunnel):
     visit = [[0]*M for _ in range(N)]
     can = 0
-    loc = deque([[R, C, 1]])
+    loc = Queue()
+    loc.put([R, C, 1])
     while loc:
-        y, x, num = loc.popleft()
+        y, x, num = loc.get()
         if not visit[y][x]:
             visit[y][x] = 1
             can += 1
@@ -29,16 +30,16 @@ def escape(N, M, R, C, L, tunnel):
                 y2, x2 = y+dy[i], x+dx[i]
                 if 0<=y2<N and 0<=x2<M and tunnel[y2][x2] and not visit[y2][x2]:
                     if i == 1 and 2 in pipe[tunnel[y2][x2]]:
-                        loc.append([y2, x2, num+1])
+                        loc.put([y2, x2, num+1])
                     elif i == 2 and 1 in pipe[tunnel[y2][x2]]:
-                        loc.append([y2, x2, num+1])
+                        loc.put([y2, x2, num+1])
                     elif i == 3 and 4 in pipe[tunnel[y2][x2]]:
-                        loc.append([y2, x2, num+1])
+                        loc.put([y2, x2, num+1])
                     elif i == 4 and 3 in pipe[tunnel[y2][x2]]:
-                        loc.append([y2, x2, num+1])
+                        loc.put([y2, x2, num+1])
     return can
 
 for tc in range(1, int(input())+1):
     N, M, R, C, L = map(int, input().split())
     tunnel = [list(map(int, input().split())) for _ in range(N)]
-    print(f'#{tc} {escape(N, M, R, C, L, tunnel)}')
+    print(f'#{tc} {run(N, M, R, C, L, tunnel)}')
