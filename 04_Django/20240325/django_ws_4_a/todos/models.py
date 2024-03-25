0b072515791d5e99fefb9671f466fc9022a1a3d9
김종덕 <kjd742388@gmail.com>
2024-03-25 17:53:35 +0900

    20240325 강의장에서 공부

---
+++ 04_Django/20240325/django_ws_4_a/todos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    work = models.CharField(max_length=100)
+    content = models.TextField()
+    is_completed = models.BooleanField()
+    # 상속 받은 Model 클래스에는 
+    # objects 라는 manager가
+    # 정의되어 있다.
+
+    def __str__(self):
+        return f'{self.pk}번째 할 일은 {self.work}입니다.'
--- algorithm_study/other/9663_N_Queen/sol.py
+++ algorithm_study/other/9663_N_Queen/sol.py
@@ -1,4 +1,25 @@
+def f(i, j, board):
+    global cnt
+    for go in range(1, i+1):
+        for k in range(3):
+            if 0 <= j+dy[k]*go < N:
+                if board[i+dx[k]*go][j+dy[k]*go]:
+                    return
+    if i == N-1:
+        cnt += 1
+        return
+    for k in range(N):
+        board[i+1][k] = True
+        f(i+1, k, board)
+        board[i+1][k] = False
+
 N = int(input())
-chess = [[0]*N for _ in range(N)]
-dx = [1, 1, 1]
+board = [[False]*N for _ in range(N)]
+dx = [-1, -1, -1]
 dy = [-1, 0, 1]
+cnt = 0
+for j in range(N):
+    board[0][j] = True
+    f(0, j, board)
+    board[0][j] = False
+print(cnt)
