79d01e507a7ed03a295d4d4fc6315f116117b5d5
김종덕 <kjd742388@gmail.com>
2024-03-28 17:58:38 +0900

    20240328 강의장에서 공부

---
+++ 04_Django/20240328/django_ws_7_1/articles/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            page = form.save()
+            return redirect('articles:detail', page.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+def detail(request, pk):
+    page = Article.objects.get(pk=pk)
+    context = {
+        'page': page,
+    }
+    return render(request, 'articles/detail.html', context)
