9cad67ef2195de7143b1afe0f3f17a956063dbbc
김종덕 <kjd742388@gmail.com>
2024-04-08 17:54:37 +0900

    20240408 강의장에서 공부

---
+++ algorithm_study/other/13913_hide_and_seek/sol.py
@@ -0,0 +1,25 @@
+def f(num, cnt, stack):
+    global min_cnt
+    if num == K:
+        if min_cnt > cnt:
+            min_cnt = cnt
+            answer = stack
+            return answer
+    elif num > K:
+        if min_cnt > cnt + num - K:
+            min_cnt = cnt + num - K
+            answer = stack + list(range(num-1, K-1, -1))
+            return answer
+    else:
+        f(num*2, cnt+1, stack + [num*2])
+        f(num+1, cnt+1, stack + [num+1])
+        f(num-1, cnt+1, stack + [num-1])
+
+        
+
+N, K = map(int, input().split())
+min_cnt = 100000
+stack = [N]
+answer = f(N, 0, stack)
+print(min_cnt)
+print(answer)
